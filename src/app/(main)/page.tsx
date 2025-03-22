0121c54ac868510cbbf1fc320f40b73f719d9b38
BossNatiyut10 <bossbattlefront@gmail.com>
2025-03-22 13:55:02 +0700

    test push

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,5 @@
 const textShadow = {textShadow: "2.5px 4px 0px black"}
 
-
 export default function Home() {
   return (
     <main>
@@ -8,7 +7,7 @@ export default function Home() {
       <section className="flex justify-start items-center relative min-h-[100vh]">
         <div style={textShadow} className="relative z-10 pl-[5rem] text-(--color-text-light)">
           <h5 className="font-bold text-3xl">Welcome to</h5>
-          <h1 className="font-bold text-[5rem]">The Massage Shop</h1>
+          <h1 className="font-bold text-[4rem]">The Massage Shop</h1>
         </div>
       </section>
     </main>
