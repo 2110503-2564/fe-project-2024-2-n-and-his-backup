cac74b4c30738da274d7d79ec83ddf690e996cec
BossNatiyut10 <bossbattlefront@gmail.com>
2025-03-25 23:19:39 +0700

    Update branch detail Page

--- src/app/(main)/branches/[bid]/page.tsx
+++ src/app/(main)/branches/[bid]/page.tsx
@@ -9,7 +9,7 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
     title: "Branch"
-};
+  };
 
 export default async function ViewBranch({params} : {params: Promise<{bid: string}>}) {
     const { bid } = await params;
@@ -23,8 +23,8 @@ export default async function ViewBranch({params} : {params: Promise<{bid: strin
     return (
     <>
         <NavBar/>
-        <div className="text-white w-full h-full pt-[80px]">
-            <div className="flex h-full w-full p-4 gap-4">
+        <div className="text-white w-full h-full pt-[60px]">
+            <div className="flex h-full w-full p-15 gap-4">
                 <div className="w-[35%] flex flex-col gap-4">
                     <Image
                     src="/img/2.png"
@@ -38,12 +38,12 @@ export default async function ViewBranch({params} : {params: Promise<{bid: strin
                         loading="lazy"
                         src={`https://www.openstreetmap.org/export/embed.html?bbox=${lon - 0.003},${lat - 0.003},${lon + 0.003},${lat + 0.003}&layer=mapnik&marker=${lat},${lon}`}>
                     </iframe>
-
                 </div>
-                <div className="relative w-[65%]">
-                    <h1 className="text-4xl font-bold">{branch.name}</h1>
-                    <h2 className='text-2xl m-2 ml-4'>Starting from <b>500฿</b></h2>
-                    <div className="p-3 space-y-3 text-xl">
+
+                <div className="relative w-[65%] flex justify-start items-center flex-col mt-[.5rem]">
+                    <h1 className="text-[3rem] font-bold">{branch.name}</h1>
+                    <h2 className='text-2xl m-2 ml-4 mt-5'>Starting from <b className='text-amber-500'>500฿</b></h2>
+                    <div className="p-3 space-y-3 text-xl flex items-start flex-col w-[70%] gap-3">
                         <div className='space-x-4'>
                             <i className="fa-solid fa-location-dot"></i>
                             <div className='inline'>{branch.address}</div>
@@ -57,22 +57,23 @@ export default async function ViewBranch({params} : {params: Promise<{bid: strin
                             <div className='inline'>{branch.openTime} - {branch.closeTime}</div>
                         </div>
                     </div>
+
                     <div className="mt-5 flex flex-wrap overflow-x-auto gap-5">
                         {
                             branch.service.map((item: any) => (
-                                <div key={item.name} className="border-2 px-10 rounded p-2 hover:bg-white hover:text-black duration-200 text-nowrap">
+                                <div key={item.name} className="border-2 px-10 rounded p-2 hover:bg-amber-600 hover:text-black duration-200 text-nowrap">
                                     <h1 className='text-center font-bold text-xl'>{item.name}</h1>
                                     <h1 className='text-center font-bold text-md'>{item.rate}฿ / hr</h1>
                                 </div>
                             ))
                         }
                     </div>
-                    <Link href={"/appointment/new?branch=" + bid} className="px-5 py-1 bg-green-600 rounded text-xl font-bold absolute bottom-5 left-0 right-0 m-auto w-fit cursor-pointer
-                    hover:bg-green-400 duration-150 text-center">
+
+                    <Link href={"/appointment/new?branch=" + bid} className="px-8 py-2 bg-amber-600 rounded text-xl font-bold absolute bottom-5 left-0 right-0 m-auto w-fit cursor-pointer
+                    hover:bg-amber-300 hover:text-black duration-150 text-center">
                     Create Appointment<br/>on this Branch
                     </Link>
                 </div>
-                
             </div>
         </div>
     </>
